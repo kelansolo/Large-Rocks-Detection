3be1ecb0bd28ac23af45a0d062f56b989559f36f
HOT <theo.houle@epfl.ch>
2024-11-22 11:39:22 +0100

    improve circling script to see mutltiple images

--- scripts/circle_rocks_on_image.py
+++ scripts/circle_rocks_on_image.py
@@ -1,20 +1,33 @@
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
+import os
 
-img_name = '2781_1141_3_0'
-
-f, ax = plt.subplots(1,1)
-
-img = plt.imread(f'data/{img_name}.tif')
-ax.imshow(img)
+dir = 'data'
+img_dir = os.path.join(dir, 'images')
 
 
-with open(f'data/{img_name}.txt', 'r') as f:
-    bboxes = f.readlines()
-    for box in bboxes:
-        x,y = box.split(' ')[1:3] 
-        x = int(float(x) * 640)
-        y = int(float(y) * 640)
-        # Draw the square as a patch (red square) of size 20 pixel (10m)
-        rect = patches.Circle((x, y), radius=20,  linewidth=2, edgecolor='r', facecolor='none')         
-        ax.add_patch(rect)
+
+images = os.listdir(img_dir)
+
+for image in images:
+    f, ax = plt.subplots(1,1)
+    img_name = image.split(".")[0]
+    img_path = os.path.join(img_dir, f'{img_name}.tif')
+    lbl_path = os.path.join(dir, "labels", f'{img_name}.txt')
+
+    img = plt.imread(img_path)
+    ax.imshow(img)
+
+
+
+    with open(lbl_path, 'r') as f:
+        bboxes = f.readlines()
+        for box in bboxes:
+            x,y = box.split(' ')[1:3] 
+            x = int(float(x) * 640)
+            y = int(float(y) * 640)
+            # Draw the square as a patch (red square) of size 20 pixel (10m)
+            rect = patches.Circle((x, y), radius=20,  linewidth=2, edgecolor='r', facecolor='none')         
+            ax.add_patch(rect)
+
+        plt.show()

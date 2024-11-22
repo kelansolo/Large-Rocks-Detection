49781ebef335432a6bccb2b9c26e155af65c29ad
kelansolo <kelansolomon@gmail.com>
2024-11-22 10:10:31 +0100

    fix: modified split.py with theo's comments

--- scripts/split.py
+++ scripts/split.py
@@ -16,17 +16,6 @@ parser.add_argument('-f', '--fractions', default= [0.6,0.2,0.2], type=float,  na
 parser.add_argument('-s', '--size', default= 0, type=int, help = 'How many images to take (e.x. if you want to test the pipeline on 10 images). \n Default is *all*')
 
 
-
-def files_empty(output_dir):
-    for root, _, files in os.walk(output_dir):
-        for file in files:
-            file_path = os.path.join(root, file)
-            # Check if the file is empty
-            if os.path.isfile(file_path) and os.path.getsize(file_path) != 0:
-                return False
-    return True
-
-
 def format_dirs(input_dir,output_dir): #Format directories
     input_dir = os.path.normpath(input_dir)
     output_dir = os.path.normpath(output_dir)
@@ -41,7 +30,6 @@ def format_dirs(input_dir,output_dir): #Format directories
 def split(input_dir, output_dir, fract, dataset_size):
     files = os.listdir(input_dir)
     # Shuffle the list of image filenames
-    random.seed(42)
     random.shuffle(files)
 
     train_size = int(len(files) * fract[0])
@@ -59,14 +47,14 @@ def split(input_dir, output_dir, fract, dataset_size):
         elif i < train_size + val_size + test_size:      
             dest_folder = "test"
         else:
-            continue
+            break
         shutil.copy(os.path.join(input_dir, f), os.path.join(output_dir, os.path.join(dest_folder+"/images", f)))
         
 
     # get actual file sizes
-    test_size = len(os.listdir(os.path.join(output_dir, "test/images")))
-    train_size = len(os.listdir(os.path.join(output_dir, "train/images")))
-    val_size = len(os.listdir(os.path.join(output_dir, "val/images")))
+    test_size = len(os.listdir(os.path.join(output_dir, "test","images")))
+    train_size = len(os.listdir(os.path.join(output_dir, "train","images")))
+    val_size = len(os.listdir(os.path.join(output_dir, "val","images")))
 
     return train_size, val_size, test_size
 
@@ -83,12 +71,8 @@ def main():
         try:
             input_dir,output_dir = format_dirs(input_dir,output_dir)
         except FileExistsError:
-            response = input("Output folders already exist, would you like to overwite them (y/n): ").strip().lower()
-            if response == "y":
-                shutil.rmtree(output_dir)
-                input_dir,output_dir = format_dirs(input_dir,output_dir)
-            else:
-                return
+            print("File is not empty, please empty it and rerun")
+            return
 
         except OSError as e:
             print(f"An error occurred: {e}")

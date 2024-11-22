beb71f98673926d6af545666d2547f5d9fa2a99c
kelansolo <kelansolomon@gmail.com>
2024-11-22 11:05:37 +0100

    fixed: theo's final suggestions

--- scripts/split.py
+++ scripts/split.py
@@ -21,7 +21,7 @@ def format_dirs(input_dir,output_dir): #Format directories
     output_dir = os.path.normpath(output_dir)
 
     # Create the directories
-    os.makedirs(os.path.join(output_dir,  "train", "images") exist_ok=False)
+    os.makedirs(os.path.join(output_dir,  "train", "images"), exist_ok=False)
     os.makedirs(os.path.join(output_dir, "val", "images"), exist_ok=False)
     os.makedirs(os.path.join(output_dir, "test", "images"), exist_ok=False)
 
@@ -32,12 +32,14 @@ def split(input_dir, output_dir, fract, dataset_size):
     # Shuffle the list of image filenames
     random.shuffle(files)
 
+    if dataset_size != 0:
+        files = files[0:dataset_size]
+
+
     train_size = int(len(files) * fract[0])
     val_size = int(len(files) * fract[1])
     test_size = int(len(files) * fract[2])
 
-    if dataset_size != 0:
-        files = files[0:dataset_size]
 
     for i, f in enumerate(files):
         if i < train_size:
@@ -64,25 +66,24 @@ def main():
     dataset_size = args.size
     input_dir = args.input_dir
     output_dir = args.output_dir
+    print(fract)
 
-    if fract[0]+fract[1]+fract[2] > 1:
-        print("Sum of fractiions can't be larger than 1")
-    else:
-        try:
-            input_dir,output_dir = format_dirs(input_dir,output_dir)
-        except FileExistsError:
-            print("File is not empty, please empty it and rerun")
-            return
+    assert fract[0]+fract[1]+fract[2] <= 1 , "Sum of fractiions can't be larger than 1"
+    try:
+        input_dir,output_dir = format_dirs(input_dir,output_dir)
+    except FileExistsError:
+        print("File is not empty, please empty it and rerun")
+        return
 
-        except OSError as e:
-            print(f"An error occurred: {e}")
-            return
+    except OSError as e:
+        print(f"An error occurred: {e}")
+        return
         
-        train_size, val_size, test_size = split(input_dir, output_dir, fract, dataset_size)
-        print("--- Split finished ---")
-        print("Train size: {}".format(train_size))
-        print("Validation size: {}".format(val_size))
-        print("Test size: {}".format(test_size))    
+    train_size, val_size, test_size = split(input_dir, output_dir, fract, dataset_size)
+    print("--- Split finished ---")
+    print("Train size: {}".format(train_size))
+    print("Validation size: {}".format(val_size))
+    print("Test size: {}".format(test_size))    
 
 if __name__ == "__main__":
     main()

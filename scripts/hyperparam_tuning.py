ea042eaa2acf6e44088f6765caf36d1c7fcc4ebd
matthiasbrosch <matthias.brosch99@gmail.com>
2024-12-03 12:44:45 +0100

    script for tuning of hyperparameters

--- scripts/hyperparam_tuning.py
+++ scripts/hyperparam_tuning.py
@@ -1,8 +1,8 @@
-pip install ultralytics
 from ultralytics import YOLO
 
 # Initialize the YOLO model
 model = YOLO("yolo11n.pt")
 
-# Tune hyperparameters on COCO8 for 30 epochs
-model.tune(data="C:\Users\matth\datasets\data\Pierre.yaml", epochs=30, iterations=300, optimizer="AdamW", plots=False, save=False, val=False)
+# Tune hyperparameters on Pierre for 30 epochs
+model.tune(data="./datasets/data/Pierre.yaml", epochs=30, iterations=300, optimizer="AdamW", plots=False, save=False, val=False)
+

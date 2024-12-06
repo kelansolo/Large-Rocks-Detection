93108817d05b6484418bcbce096cb2253ddec3c8
matthiasbrosch <matthias.brosch99@gmail.com>
2024-12-06 09:59:45 +0100

    changed to yolov8

--- scripts/hyperparam_tuning.py
+++ scripts/hyperparam_tuning.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
 # Initialize the YOLO model
-model = YOLO("yolo11n.pt")
+model = YOLO("yolov8n.pt")
 
 # Tune hyperparameters on Pierre for 30 epochs
-model.tune(data="./datasets/data/Pierre.yaml", epochs=30, iterations=300, optimizer="AdamW", plots=False, save=False, val=False)
+model.tune(data=".\Pierre.yaml", epochs=30, iterations=300, optimizer="AdamW", plots=False, save=False, val=False)
 
